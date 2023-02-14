4d65a4abcc572354fac7010d421095e362188a00
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2023-02-13 21:39:29 -0400

    Add screen limits

---
+++ src/Level/Level1.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const LIMIT_LEFT = -2000
+const LIMIT_TOP = -2000
+const LIMIT_RIGHT = 4000
+const LIMIT_BOTTOM = 2000
+
+func _ready():
+	for child in get_children():
+		if child is Player:
+			var camera = child.get_node("Camera")
+			camera.limit_left = LIMIT_LEFT
+			camera.limit_top = LIMIT_TOP
+			camera.limit_right = LIMIT_RIGHT
+			camera.limit_bottom = LIMIT_BOTTOM
